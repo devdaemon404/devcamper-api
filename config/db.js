58ec8a0ce7480c9058b6eb422fa468f3dd4f4e4a
Bharath Karanth <bharathkaranth07@gmail.com>
2020-06-10 13:56:55 +0530

    Added option to mongoose connect function to handle popping errors

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useCreateIndex:true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
