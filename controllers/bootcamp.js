96e2262f8510c1760ceab2b9294b63643b608c97
Bharath Karanth <bharathkaranth07@gmail.com>
2020-06-14 18:33:20 +0530

    Added getCourse, addCourse ,updateCourse and deleteCourse routers and controllers

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -29,7 +29,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   );
 
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
   if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
@@ -135,13 +135,14 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
  * @access  Private
  */
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
-  res.status(200).json({ success: true, data: bootcamp });
+  bootcamp.remove();
+  res.status(200).json({ success: true, data: {} });
 });
 
 /**
