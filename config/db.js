096ab8934044a43276c484acd8f34911a4f54148
Bharath Karanth <bharathkaranth07@gmail.com>
2020-06-14 18:32:21 +0530

    Added useFindAndModify option

--- config/db.js
+++ config/db.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useCreateIndex:true,
+    useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   });
   console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
 };
