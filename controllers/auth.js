788ae860e36b34600af36c7700ad66d13c1477dc
Bharath Karanth <devdaemon404@gmail.com>
2020-07-18 12:36:03 +0530

    Added function to send JWT as cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,10 +18,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  //create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 /**
@@ -39,7 +36,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 
   // Check for user
   const user = await User.findOne({ email }).select('+password');
-  
+
   if (!user) {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
@@ -51,8 +48,24 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
-  //create token
+  sendTokenResponse(user, 200, res);
+});
+
+//Get token from model ,create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  });
+};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('morgan');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -24,6 +25,8 @@ const app = express();
 
 //Body parser for parsing json
 app.use(express.json());
+
+app.use(cookieParser());
 
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
