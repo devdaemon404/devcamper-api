920ae334385d5b358fdad0566f4044aa3d8ad6d0
Bharath Karanth <devdaemon404@gmail.com>
2020-07-18 21:16:10 +0530

    Added sendEmail utils Added resetPassword functionality

--- middleware/auth.js
+++ middleware/auth.js
@@ -21,7 +21,6 @@ exports.protect = asyncHandler(async (req, res, next) => {
   }
   try {
     //Verify token
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById({ _id: decoded.id });
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { register, login, getMe ,forgotPassword} = require('../controllers/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+} = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
@@ -12,5 +18,6 @@ router.get('/me', protect, getMe);
 
 router.post('/forgotPassword', forgotPassword);
 
+router.put('/resetPassword/:resetToken', resetPassword);
 
 module.exports = router;
